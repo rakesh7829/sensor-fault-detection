32dbe7275a6664fe7881c5f35a137b7758b17c0f
rakesh naragund <rakeshnaragund@yahoo.com>
2023-01-27 12:04:20 +0530

    setup project structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+
+
+#def get_requirements()-> List[str]:
+
+
+   # """
+   # This function returns list of requirements
+   # """
+
+
+    #requirement_list=['requirements.txt']
+    #return requirement_list
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='RAKESH',
+    author_email='nargundrakesh60@gmail.com',
+    packages=find_packages(),
+    
+
+
+)
