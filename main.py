206135da38953333cc776bc2ab663de7996a830e
rakesh naragund <rakeshnaragund@yahoo.com>
2023-01-31 11:42:51 +0530

    updated

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os,sys
+
+def test_exception():
+    try:
+        x=1/0
+    except Exception as e:
+        raise SensorException(e,sys)
+
 
 
 
 if __name__=='__main__':
-    mongodb_client=MongoDBClient()
-    print(mongodb_client.database.list_collection_names())
+    #mongodb_client=MongoDBClient()
+    #print(mongodb_client.database.list_collection_names())
+    try:
+       test_exception()
+    except Exception as e:
+        print(e)
